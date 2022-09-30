4bd2ab79cc259e02e86604d2ce73398d18acb3dd
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-30 12:19:56 -0700

    Extract header component and add title

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import { NavLink, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import GDPChart from './components/Chart/Chart';
+import Header from './components/Header/Header';
 import Table from './components/Table/Table';
 
 function App() {
   return (
     <div className='grid grid-rows-page-layout'>
-      <nav className='p-2 flex justify-end gap-1 bg-slate-600 shadow-xl'>
-        <NavLink to="/table" className='shadow-lg bg-cyan-800 text-white px-2 py-1 rounded [&.active]:bg-cyan-700'>
-          Table
-        </NavLink>
-        <NavLink to="/charts" className='shadow-lg bg-cyan-800 text-white px-2 py-1 rounded [&.active]:bg-cyan-700'>
-          Charts
-        </NavLink>
-      </nav>
+      <Header />
       <Switch>
         <Route path="/table">
           <div className='flex flex-col'>
-            <h1 className='m-3 text-3xl font-bold'>React Suite Table Example</h1>
+            <h1 className='m-3 text-3xl font-bold'>React Suite Table</h1>
             <Table />
           </div>
         </Route>
         <Route path="/charts">
           <div className='flex flex-col'>
-            <h1 className='m-3 text-3xl font-bold'>Recharts Example</h1>
+            <h1 className='m-3 text-3xl font-bold'>Recharts</h1>
             <GDPChart countries={['USA', 'CHN', 'IND', 'JPN', 'DEU']} />
           </div>
         </Route>
